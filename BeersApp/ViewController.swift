6b4d6277449e49ff27ce2573af66c0a177f048dd
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-03-17 10:13:59 +0300

    Initial Commit

---
+++ BeersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeersApp
+//
+//  Created by Aleksey Konchyts on 17.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
